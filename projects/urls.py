e46998dd09bca28fe16eab4eb0ab7d441ff4e739
raxfor <jhonalexslb@gmail.com>
2022-09-13 21:13:35 -0500

    first commit

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .api import ProjectsViewSet
+router = routers.DefaultRouter()
+
+router.register('api/projects', ProjectsViewSet, 'projects')
+
+urlpatterns = router.urls
+
